f02323f89d2fa12e6afef07e161314b17270ff24
SeanCarrick <PekinSOFT@outlook.com>
2019-10-01 21:55:54 -0500

    `CLArguments` Class Completed :: Untested
    I have completed coding the logic in the `CLArguments` class. Next, I'm going to work on some JUnit tests to test the class to be sure that it works as expected.

--- src/com/is2300/clargparser/CLArguments.java
+++ src/com/is2300/clargparser/CLArguments.java
@@ -17,6 +17,8 @@
 
 package com.is2300.clargparser;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -159,6 +161,203 @@ public class CLArguments {
         }
         
         return val == null ? defaultValue : val;
+    }
+    
+    /**
+     * Retrieves the value for the given switch as a {@code java.lang.Long}.
+     * 
+     * @param switchName    The switch for which value is wanted.
+     * @return              {@code java.lang.Long} value of the switch value.
+     * @throws              {@code NumberFormatException} if the string does not 
+     *                      contain a parsable long.
+     */
+    public Long getSwitchLongValue(String switchName) {
+        return getSwitchLongValue(switchName, null);
+    }
+    
+    /**
+     * Retrieves the value for the given switch as a {@code java.lang.Long}, or
+     * the provided default value if the given switch is not found.
+     * 
+     * @param switchName    The switch for which value is wanted.
+     * @param defaultValue  A default value in the event the switch is not
+     *                      present.
+     * @return              {@code java.lang.Long} value, if the switch is 
+     *                      present; the supplied default value if the switch is
+     *                      not found.
+     * @throws              {@code NumberFormatException} if the string does not 
+     *                      contain a parsable long.
+     */
+    public Long getSwitchLongValue(String switchName, Long defaultValue) {
+        String switchValue = getSwitchValue(switchName, null);
+        
+        if ( switchValue == null ) {
+            return defaultValue;
+        }
+        
+        return Long.parseLong(switchValue);
+    }
+    
+    /**
+     * Retrieves the value for the given switch as a {@code java.lang.Double}.
+     * 
+     * @param switchName    The switch for which value is wanted.
+     * @return              {@code java.lang.Double} value of the switch value.
+     * @throws              {@code NumberFormatException} if the string does not 
+     *                      contain a parsable double.
+     */
+    public Double getSwitchDoubleValue(String switchName) {
+        return getSwitchDoubleValue(switchName, null);
+    }
+    
+    /**
+     * Retrieves the value for the given switch as a {@code java.lang.Double}, 
+     * or the provided default value if the given switch is not found.
+     * 
+     * @param switchName    The switch for which value is wanted.
+     * @param defaultValue  A default value in the event the switch is not
+     *                      present.
+     * @return              {@code java.lang.Double} value, if the switch is 
+     *                      present; the supplied default value if the switch is
+     *                      not found.
+     * @throws              {@code NumberFormatException} if the string does not 
+     *                      contain a parsable double.
+     */
+    public Double getSwitchDoubleValue(String switchName, Double defaultValue) {
+        String switchValue = getSwitchValue(switchName, null);
+        
+        if ( switchValue == null ) {
+            return defaultValue;
+        }
+        
+        return Double.parseDouble(switchValue);
+    }
+    
+    /**
+     * Retrieves an array of {@code java.lang.String} values for the given 
+     * switch.
+     * 
+     * @param switchName    The switch for which values are wanted.
+     * @return              An array of {@code java.lang.String}s containing all
+     *                      of the values associated with the given switch. If
+     *                      the switch is not found, an array of length zero (0)
+     *                      will be returned.
+     */
+    public String[] getSwitchValues(String switchName) {
+        if ( !switchIndices.containsKey(switchName) ) {
+            return new String[0];
+        }
+        
+        int switchIndex = switchIndices.get(switchName);
+        int nextArgIndex = switchIndex + 1;
+        while ( nextArgIndex < args.length 
+                && !args[nextArgIndex].startsWith("-") ) {
+            takenIndices.add(nextArgIndex);
+            nextArgIndex++;
+        }
+        
+        String[] values = new String[nextArgIndex - switchIndex - 1];
+        for ( int idx = 0; idx < values.length; idx++ ) {
+            values[idx] = args[switchIndex + idx + 1];
+        }
+        
+        return values;
+    }
+    
+    /**
+     * Retrieves the switch and its values as a Plain Old Java Object (POJO).
+     * 
+     * @param clazz The class to which the switch and values should be placed
+     *              into.
+     * @return      {@code java.lang.Object} of the switch and its values.
+     * @throws      {@code RuntimeException} in the event the POJO cannot be
+     *              created.
+     */
+    public Object getSwitchPOJO(Class<Object> clazz) {
+        try {
+            Class<?> newClass = Class.forName(clazz.getName());
+            Constructor<?> constructor = clazz.getConstructor();
+            Object pojo = constructor.newInstance();
+            Field[] fields = clazz.getFields();
+            
+            for ( Field field : fields ) {
+                Class fieldType = field.getType();
+                String fieldName = "-" + field.getName().replace("_", "-");
+                
+                if ( fieldType.equals(Boolean.class)
+                        || fieldType.equals(boolean.class) ) {
+                    field.set(pojo, isSwitchPresent(fieldName));
+                } else if ( fieldType.equals(String.class) ) {
+                    if ( getSwitchValue(fieldName) != null ) {
+                        field.set(pojo, getSwitchValue(fieldName));
+                    }
+                } else if ( fieldType.equals(Long.class) 
+                        || fieldType.equals(long.class) ) {
+                    if ( getSwitchLongValue(fieldName) != null ) {
+                        field.set(pojo, getSwitchLongValue(fieldName));
+                    }
+                } else if ( fieldType.equals(Integer.class) 
+                        || fieldType.equals(int.class) ) {
+                    if ( getSwitchLongValue(fieldName) != null ) {
+                        field.set(pojo, getSwitchLongValue(fieldName)
+                                .intValue());
+                    }
+                } else if ( fieldType.equals(Short.class) 
+                        || fieldType.equals(short.class) ) {
+                    if ( getSwitchLongValue(fieldName) != null ) {
+                        field.set(pojo, getSwitchLongValue(fieldName)
+                                .shortValue());
+                    }
+                } else if ( fieldType.equals(Byte.class) 
+                        || fieldType.equals(byte.class) ) {
+                    if ( getSwitchLongValue(fieldName) != null ) {
+                        field.set(pojo, getSwitchLongValue(fieldName)
+                                .byteValue());
+                    }
+                } else if ( fieldType.equals(Double.class) 
+                        || fieldType.equals(double.class) ) {
+                    if ( getSwitchDoubleValue(fieldName) != null ) {
+                        field.set(pojo, getSwitchDoubleValue(fieldName));
+                    }
+                } else if ( fieldType.equals(Float.class) 
+                        || fieldType.equals(float.class) ) {
+                    if ( getSwitchDoubleValue(fieldName) != null ) {
+                        field.set(pojo, getSwitchDoubleValue(fieldName)
+                                .floatValue());
+                    }
+                } else if ( fieldType.equals(String[].class) ) {
+                    String[] values = getSwitchValues(fieldName);
+                    if ( values.length != 0 ) {
+                        field.set(pojo, values);
+                    }
+                }
+            }
+            
+            return pojo;
+        } catch ( ClassNotFoundException | IllegalAccessException ex ) {
+            throw new RuntimeException("Error creating switch POJO", ex);
+        }
+    }
+    
+    /**
+     * Retrieves all targets as an array of {@code java.lang.String}s. The 
+     * targets that are returned are values that had no switch associated with
+     * them.
+     * 
+     * @return All of the targets that did not have a switch associated with
+     *         them.
+     */
+    public String[] getTargets() {
+        String[] aryTargets = new String[args.length - takenIndices.size()];
+        int targetIndex = 0;
+        
+        for ( int idx = 0; idx < args.length; idx++ ) {
+            if ( !takenIndices.contains(idx) ) {
+                aryTargets[targetIndex++] = args[idx];
+            }
+        }
+        
+        return aryTargets;
     }
     //</editor-fold>
 
